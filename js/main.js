bc91ac3dcc2c6d588b69bb55c1c315cde4bcbf6f
kyoro <hirohisaonuma@icloud.com>
2020-09-22 20:53:41 +0900

    game1

---
+++ js/main.js
@@ -0,0 +1,34 @@
+"use strict";
+
+{
+  class Panel {
+    constructor() {
+      this.el = document.createElement("li");
+      this.el.classList.add("pressed");
+    }
+
+    getEl() {
+      return this.el;
+    }
+  }
+
+  class Board {
+    constructor() {
+      this.panels = [];
+      for (let i = 0; i < 4; i++) {
+        this.panels.push(new Panel());
+      }
+      this.setup();
+    }
+
+    setup() {
+      const board = document.getElementById("board");
+      this.panels.forEach((panel) => {
+        //   board.appendChild(panel.el);
+        board.appendChild(panel.getEl());
+      });
+    }
+  }
+
+  const board = new Board();
+}
